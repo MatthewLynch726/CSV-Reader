5a7f139b81f6993e256d2650bc2074fb0d5d8122
MatthewLynch726 <70592608+MatthewLynch726@users.noreply.github.com>
2023-09-17 16:20:00 -0400

    Was able to read from a CSV file and output the result

--- CSV Reader/Program.cs
+++ CSV Reader/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CSV_Reader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var reader = new StreamReader(@"C:\Test\CSV-File.csv"))
+            {
+                Console.WriteLine("Test!");
+                List<string> listA = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+
+                    foreach (var i in values)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
