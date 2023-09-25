9556226f50c6dca77971cf9dc0b80576ad795b76
MatthewLynch726 <70592608+MatthewLynch726@users.noreply.github.com>
2023-09-25 16:29:08 -0400

    Going to try and reset the database again

--- WebApplication1/Models/CSV.cs
+++ WebApplication1/Models/CSV.cs
@@ -2,7 +2,7 @@
 {
     public class CSV
     {
-        public int Id { get; set; }
+        public int ContractId { get; set; }
         public string Name { get; set; }
         public string Value { get; set; }
     }
