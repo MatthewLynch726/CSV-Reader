6d85015e2b5aadf4898f0cced2d36f50438c0fd9
MatthewLynch726 <70592608+MatthewLynch726@users.noreply.github.com>
2023-09-25 15:35:22 -0400

    Adding ability to upload File

--- WebApplication1/Controllers/CSVsController.cs
+++ WebApplication1/Controllers/CSVsController.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class CSVsController : Controller
     {
+
+        private readonly ApplicationDbContex _db;
+
+        public CSVsController(ApplicationDbContex db)
+        {
+            _db = db;
+        }
+
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<CSV> objCategoryList = _db.CSVs.ToList();
+            return View(objCategoryList);
         }
+
+        [HttpPost]
+        public IActionResult Upload(IFormFile UploadedFile) {
+
+            Console.WriteLine("Test!");
+
+
+            return RedirectToAction("Index");
+
+        }
+
+
     }
 }
--- WebApplication1/Models/CSV.cs
+++ WebApplication1/Models/CSV.cs
@@ -3,6 +3,7 @@
     public class CSV
     {
         public int Id { get; set; }
+        public string ContractId { get; set; }
         public string Name { get; set; }
         public string Value { get; set; }
     }
