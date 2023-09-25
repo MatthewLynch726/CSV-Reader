590284507327a943746e58a2952f966e15ca2699
MatthewLynch726 <70592608+MatthewLynch726@users.noreply.github.com>
2023-09-25 14:56:10 -0400

    Adding MVC and database

---
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContex : DbContext
+    {
+        public ApplicationDbContex(DbContextOptions<ApplicationDbContex> options) : base(options)
+        {
+
+        }
+
+        public DbSet<CSV> CSVs { get; set; }
+
+    }
+}
+
---
+++ WebApplication1/Models/CSV.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class CSV
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
